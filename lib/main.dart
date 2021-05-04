67bb31e8f592dab0263d81be27d01f387571c799
MorDvash <mordvash1@gmail.com>
2021-05-04 08:03:52 +0300

    first-commit

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("DeliMeals"),
+          title: Text("DeliMeals!!!"),
         ),
         body: Center(
           child: Text('Nav Time!'),

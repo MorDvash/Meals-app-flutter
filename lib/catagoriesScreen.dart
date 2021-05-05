90aa93c2642b9c182f6f05d76cf8a53ed9919fdb
MorDvash <mordvash1@gmail.com>
2021-05-05 08:15:19 +0300

    catagoryScreenLoading

--- lib/catagoriesScreen.dart
+++ lib/catagoriesScreen.dart
@@ -6,15 +6,20 @@ import './catagoryItem.dart';
 class CatagoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-        children: DUMMY_CATEGORIES
-            .map((catData) => CatagoryItem(catData.title, catData.color))
-            .toList(),
-        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ));
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: GridView(
+          children: DUMMY_CATEGORIES
+              .map((catData) => CatagoryItem(catData.title, catData.color))
+              .toList(),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          )),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './catagoriesScreen.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -12,25 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: CatagoriesScreen(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("DeliMeals!!!"),
-        ),
-        body: Center(
-          child: Text('Nav Time!'),
-        ));
   }
 }

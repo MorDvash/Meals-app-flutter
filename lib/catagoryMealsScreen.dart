32da8b015aa95969ba4c93db1d9514de9c634091
MorDvash <mordvash1@gmail.com>
2021-05-10 07:54:30 +0300

    Show meals on selected catagory

--- lib/catagoryMealsScreen.dart
+++ lib/catagoryMealsScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './catagoryData.dart';
+
 class CatagoryMealsScreen extends StatelessWidget {
   static const routeName = '/catagory-meals';
 
@@ -9,12 +11,20 @@ class CatagoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context).settings.arguments as Map<String, String>;
     final catagoryTitle = routeArgs['title'];
     final catagoryId = routeArgs['id'];
+    final catagoryMeals = DUMMY_MEALS
+        .where((meal) => meal.categories.contains(catagoryId))
+        .toList();
     return Scaffold(
         appBar: AppBar(
           title: Text(catagoryTitle),
         ),
-        body: Center(
-          child: Text('MealsList'),
+        body: ListView.builder(
+          itemCount: catagoryMeals.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(catagoryMeals[index].title),
+            );
+          },
         ));
   }
 }
